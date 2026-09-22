cfe3ae417db683d4d4bec26bea0fa3c28e07824c
Mohammad Alhaj <mohammad.al-haj@cloudsolutions.com.sa>
2026-09-22 15:52:45 +0300

    Init

---
+++ inventions-carseer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace inventions_carseer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
